9ba8ff1b2ed69751152fe40ec887bd6ba842497d
FrancaTM <francatm@outlook.com>
2022-01-12 18:45:14 -0300

    create and connect outlets

---
+++ Today/ReminderListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by Tulio Marcos Franca on 12/01/22.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
+}
