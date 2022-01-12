0cb42a4524d85d0ea09a292a2d25432b8803da32
FrancaTM <francatm@outlook.com>
2022-01-12 19:09:53 -0300

    refactor template view controller to create table view controller

---
+++ Today/ReminderListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Tulio Marcos Franca on 09/01/22.
+//
+
+import UIKit
+
+class ReminderListViewController: UITableViewController {
+    
+    
+    
+}
+
