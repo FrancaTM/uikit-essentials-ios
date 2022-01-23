be8713cf8acc98818f862cfae456a68a128a1510
FrancaTM <francatm@outlook.com>
2022-01-23 10:37:52 -0300

    add reminderListDataSource extract data source methods from reminderListViewController

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -21,25 +21,3 @@ class ReminderListViewController: UITableViewController {
     }
 }
 
-extension ReminderListViewController {
-    static let reminderListCellIdentifier = "ReminderListCell"
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
-            fatalError("Unable to dequeue ReminderCell")
-        }
-        
-        let reminder = Reminder.testData[indexPath.row]
-        
-        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
-            Reminder.testData[indexPath.row].isComplete.toggle()
-            tableView.reloadRows(at: [indexPath], with: .none)
-        }
-        
-        return cell
-    }
-}
