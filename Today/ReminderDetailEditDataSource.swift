3489903a21beca4e642dda5febee13b7f34723bf
FrancaTM <francatm@outlook.com>
2022-01-24 09:25:35 -0300

    add detail edit data source

---
+++ Today/ReminderDetailEditDataSource.swift
@@ -0,0 +1,38 @@
+//
+//  ReminderDetailEditDataSource.swift
+//  Today
+//
+//  Created by Tulio Marcos Franca on 24/01/22.
+//
+
+import UIKit
+
+class ReminderDetailEditDataSource: NSObject {
+    enum ReminderSection: Int, CaseIterable {
+        case title
+        case dueDate
+        case notes
+        
+        var displayText: String {
+            switch self {
+            case .title:
+                return "Title"
+            case .dueDate:
+                return "Date"
+            case .notes:
+                return "Notes"
+            }
+        }
+        
+        func cellIdentifier(for row: Int) -> String {
+            switch self {
+            case .title:
+                return "EditTitleCell"
+            case .dueDate:
+                return row == 0 ? "EditDateLabelCell" : "EditDateCell"
+            case .notes:
+                return "EditNotesCell"
+            }
+        }
+    }
+}
