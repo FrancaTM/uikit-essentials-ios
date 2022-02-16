15a55584fb56cbe1ecc18a9c04632d55a67fb0a6
FrancaTM <francatm@outlook.com>
2022-02-16 20:11:07 -0300

    filter reminders by due date

--- Today/ReminderListDataSource.swift
+++ Today/ReminderListDataSource.swift
@@ -10,12 +10,37 @@ import UIKit
 class ReminderListDataSource: NSObject {
     private lazy var dateFormatter = RelativeDateTimeFormatter()
     
+    enum Filter: Int {
+        case today
+        case future
+        case all
+        
+        func shouldInclude(date: Date) -> Bool {
+            let isInToday = Locale.current.calendar.isDateInToday(date)
+            
+            switch self {
+            case .today:
+                return isInToday
+            case .future:
+                return (date > Date()) && !isInToday
+            case .all:
+                return true
+            }
+        }
+    }
+    
+    var filter: Filter = .today
+    
+    var filteredReminders: [Reminder] {
+        return Reminder.testData.filter { filter.shouldInclude(date: $0.dueDate) }.sorted { $0.dueDate < $1.dueDate }
+    }
+    
     func update(_ reminder: Reminder, at row: Int) {
         Reminder.testData[row] = reminder
     }
     
     func reminder(at row: Int) -> Reminder {
-        return Reminder.testData[row]
+        return filteredReminders[row]
     }
     
     func add(_ reminder: Reminder) {
@@ -27,7 +52,7 @@ extension ReminderListDataSource: UITableViewDataSource {
     static let reminderListCellIdentifier = "ReminderListCell"
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
+        return filteredReminders.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
